d66449eb4cef04b4835ceedc270cd984e9b6477d
Fatih <fatihealan@gmail.com>
2023-03-26 22:13:09 -0500

    Using trpc to get all the posts and display in the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -20,6 +20,11 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {user.isSignedIn && <SignOutButton />}
         </h1>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,8 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { postsRouter } from "./routers/post";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  posts: postsRouter,
 });
 
 // export type definition of API
